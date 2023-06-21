b4a9b38cce27d749b95e398a8d12fb7cbb3e8d07
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-06-22 00:49:22 +0300

    displayTimer

--- script.js
+++ script.js
@@ -23,20 +23,20 @@ document.getElementById("start-timer").addEventListener("click", () => {
 });
 
 
-// function displayTimer() {
-//     milliseconds += 10;
-//     seconds = milliseconds == 1000 ? (seconds+1) %60 : seconds
-//     minutes = seconds == 60 ? (minutes +1) %60 : minutes
-//     hours = minutes==60 ? (hours+1)%24 : hours
-//     milliseconds = milliseconds == 1000 ? 0 : milliseconds
+function displayTimer() {
+    milliseconds += 10;
+    seconds = milliseconds == 1000 ? (seconds+1) %60 : seconds
+    minutes = seconds == 60 ? (minutes +1) %60 : minutes
+    hours = minutes==60 ? (hours+1)%24 : hours
+    milliseconds = milliseconds == 1000 ? 0 : milliseconds
 
-//     let h = String(hours).padStart(2, "0");
-//     let m = String(minutes).padStart(2, "0");
-//     let s = String(seconds).padStart(2, "0");
-//     let ms = String(milliseconds).padStart(3, "0");
+    let h = String(hours).padStart(2, "0");
+    let m = String(minutes).padStart(2, "0");
+    let s = String(seconds).padStart(2, "0");
+    let ms = String(milliseconds).padStart(3, "0");
 
-//     timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
-// }
+    timerRef.innerHTML = `${h} : ${m} : ${s} : ${ms}`;
+}
 
 // document.getElementById("pause-timer").addEventListener("click", () => {
 //     clearInterval(flag);
